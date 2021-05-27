2cb2b7f86c2be87332273cfa01815c91996f0206
rage875 <rage.875@gmail.com>
2021-05-27 13:57:03 -0500

    Estadisticas: media, varianza, desviacion estandard

--- estaditicas.py
+++ estaditicas.py
@@ -1,11 +1,33 @@
 import random
 
-def media(x):
-    return sum(x) / len(x)
+def media(X):
+    # Promedio de los datos
+    return sum(X) / len(X)
 
-if __name__ == "__main__":
-    X = [random.randint(1,21) for i in range(20)]
+def varianza(X):
+    # (1/n)(sum(X(i)-mu)^2)
     mu = media(X)
 
-    print(X)
-    print(mu)
+    res = 0
+    for x in X:
+        res += (x - mu) ** 2
+
+    return res / len(X)
+
+def desviacion_estandar(X):
+    # Raiz cuadrada de la varianza
+    return varianza(X) ** 0.5
+
+
+if __name__ == "__main__":
+    # X - Se refiere a un arreglo de datos
+    # mu - Media
+    X = [random.randint(1,21) for i in range(20)]
+    mu = media(X)
+    var = varianza(X)
+    sigma = desviacion_estandar(X) # Que tanta variabilidad habra entre los datos
+
+    print(f"Arreglo de datos: {X}")
+    print(f"Media: {mu}")
+    print(f"Varianza: {var}")
+    print(f"Desviacion estandar: {sigma}")
